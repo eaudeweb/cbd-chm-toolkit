4506444c9bcd12caa32d13a5431f20dd6a4e4667
Cristian Romanescu <cristian@romanescu.ro>
2016-04-19 00:55:05 +0300

    Create the targets and strategic goals during module installation

--- docroot/modules/chm/ptk_base/src/Tests/PTKBaseTest.php
+++ docroot/modules/chm/ptk_base/src/Tests/PTKBaseTest.php
@@ -26,15 +26,30 @@ class PTKBaseTest extends WebTestBase {
    */
   public static $modules = array('field_ui', 'ptk_base');
 
-  public function testTaxonomyCreation() {
+  function setUp() {
+    parent::setUp();
     // Log in an admin user.
     $admin_user = $this->drupalCreateUser(array(
       'administer taxonomy' , 'administer taxonomy_term fields'
     ));
     $this->drupalLogin($admin_user);
+  }
 
-    // Get a list of content types.
+  public function testTaxonomyCreation() {
     $this->drupalGet('/admin/structure/taxonomy');
     $this->assertText('Aichi biodiversity target', 'Aichi biodiversity target taxonomy found');
+  }
+
+  public function testTaxonomyTermData() {
+    $this->drupalGet('/admin/structure/taxonomy/manage/aichi_target/overview');
+    $this->assertText('Aichi Strategic Goal A');
+    $this->assertText('Aichi Strategic Goal B');
+    $this->assertText('Aichi Strategic Goal C');
+    $this->assertText('Aichi Strategic Goal D');
+    $this->assertText('Aichi Strategic Goal E');
+
+    foreach(range(1, 20) as $k) {
+      $this->assertText('Aichi Target ' . $k);
+    }
   }
 }
