ad7c0ecb1d65d88649546a634e21f5d82ad0b53a
Cristian Romanescu <cristian@romanescu.ro>
2016-04-24 23:21:55 +0300

    Generate Excel report

--- docroot/modules/chm/ptk_base/src/Controller/ContentTypesOverviewController.php
+++ docroot/modules/chm/ptk_base/src/Controller/ContentTypesOverviewController.php
@@ -8,16 +8,21 @@
 namespace Drupal\ptk_base\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Field\Annotation\FieldType;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Field\FieldTypePluginManager;
-use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Core\Link;
 
 
 class ContentTypesOverviewController extends ControllerBase {
 
   public function content() {
-    $ret = [];
+    $d = Link::createFromRoute($this->t('Download Excel report'), 'ptk_base.admin.content_types_overview.export');
+    $ret = [
+      'download' => [
+        '#type' => 'markup',
+        '#markup' => $d->toString()
+      ],
+    ];
 
     $ctypes = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
     ksort($ctypes);
@@ -63,4 +68,69 @@ class ContentTypesOverviewController extends ControllerBase {
     return $ret;
   }
 
+  public function export() {
+    module_load_include('inc', 'phpexcel');
+    libraries_load('PHPExcel');
+
+    // Create new PHPExcel object
+    $objPHPExcel = new \PHPExcel();
+    // Set document properties
+    $objPHPExcel->getProperties()->setCreator("CHM portal")
+      ->setLastModifiedBy("CHM portal")
+      ->setTitle("Content types overview")
+      ->setSubject("Content types overview")
+      ->setDescription("Export of the content types defined in the CHM portal")
+      ->setKeywords("drupal structure");
+
+
+    $ctypes = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
+    ksort($ctypes);
+
+    /** @var FieldTypePluginManager $fts */
+    $fts = \Drupal::service('plugin.manager.field.field_type');
+    $field_definitions = $fts->getDefinitions();
+    $objPHPExcel->setActiveSheetIndex(0);
+    $i = 0;
+    foreach ($ctypes as $machine_name => $ctype) {
+      $j = 1;
+      $sheet = new \PHPExcel_Worksheet();
+      $sheet->setTitle($machine_name);
+      $fields = \Drupal::service('entity_field.manager')->getFieldDefinitions('node', $machine_name);
+      /** @var BaseFieldDefinition $field_data */
+      foreach ($fields as $field_name => $field_data) {
+        if ($j == 1) {
+          $sheet->setCellValue('A1', 'Field name');
+          $sheet->setCellValue('B1', 'Machine name');
+          $sheet->setCellValue('C1', 'Description');
+          $sheet->setCellValue('D1', 'Data type');
+        }
+        $fd = $field_definitions[$field_data->getType()];
+        if ($s = $field_data->getLabel()) {
+          $sheet->setCellValue('A' . $j, is_object($s) ? $s->render() : $s);
+        }
+        if ($s = $field_data->getName()) {
+          $sheet->setCellValue('B' . $j, is_object($s) ? $s->render() : $s);
+        }
+        if ($s = $field_data->getDescription()) {
+          $sheet->setCellValue('C' . $j, is_object($s) ? $s->render() : $s);
+        }
+        if (!empty($fd['label']) && $label = $fd['label']) {
+          $sheet->setCellValue('D' . $j, is_object($label) ? $label->render() : $label);
+        }
+        $j++;
+      }
+      $objPHPExcel->addSheet($sheet);
+      $i++;
+    }
+    $objPHPExcel->removeSheetByIndex(0);
+    // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+    $objPHPExcel->setActiveSheetIndex(0);
+    // Redirect output to a client’s web browser (Excel2007)
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment;filename="content_types_overview.xlsx"');
+    header('Cache-Control: max-age=0');
+    $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $objWriter->save('php://output');
+    die();
+  }
 }
