6acacccdb0a739cca8bf75e420c183e7d1af94c1
Cristian Romanescu <cristian@romanescu.ro>
2016-05-02 23:23:57 +0300

    Implemented country per domain settings

--- docroot/sites/all/modules/chm/ptk_base/include/PTK.php
+++ docroot/sites/all/modules/chm/ptk_base/include/PTK.php
@@ -3,6 +3,37 @@
 
 class PTK {
 
+  public static function getPortalCountry() {
+    $domain = domain_get_domain();
+    $realm_key = _domain_variable_realm_key($domain['machine_name']);
+    if ($iso = variable_realm_get('domain', $realm_key, 'country')) {
+      return self::getCountryByCode($iso);
+    }
+    return NULL;
+  }
+
+  public static function getCountryListAsOptions() {
+    $ret = [];
+    $countries = self::getCountryList();
+    foreach ($countries as $country) {
+      $w = entity_metadata_wrapper('taxonomy_term', $country);
+      $code = $w->field_country_code->value();
+      $ret[$code] = $w->label();
+    }
+    return $ret;
+  }
+
+
+  public static function getCountryList() {
+    $items = drupal_static(__METHOD__);
+    if (!isset($items)) {
+      $v = taxonomy_vocabulary_machine_name_load('countries');
+      $items = taxonomy_term_load_multiple(NULL, array('vid' => $v->vid));
+    }
+    return $items;
+  }
+
+
   /**
    * Retrieve a country by its code.
    *
@@ -11,13 +42,13 @@ class PTK {
    * @return stdClass|NULL
    *   Country object
    */
-  static function getCountryByCode($iso) {
+  public static function getCountryByCode($iso) {
     $items = drupal_static(__METHOD__);
     if (!isset($items)) {
       $q = db_select('field_data_field_country_code');
       $q->fields(NULL, array('entity_id', 'field_country_code_value'));
       $q->condition('entity_type', 'taxonomy_term');
-      $q->condition('bundle', 'items');
+      $q->condition('bundle', 'countries');
       $rows = $q->execute()->fetchAllKeyed();
       foreach ($rows as $k => $code) {
         $items[$code] = taxonomy_term_load($k);
@@ -39,7 +70,7 @@ class PTK {
    * @return stdClass|NULL
    *   The corrersponding taxonomy term
    */
-  static function getNFPTypeByAcronym($acronym) {
+  public static function getNFPTypeByAcronym($acronym) {
     $items = drupal_static(__METHOD__);
     if (!isset($items)) {
       $q = db_select('field_data_field_original_id');
