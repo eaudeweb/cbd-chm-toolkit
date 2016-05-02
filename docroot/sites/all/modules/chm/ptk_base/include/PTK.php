a4ec6cf7eb6f6040d9d07ccc2d5a569ba4a10360
Cristian Romanescu <cristian@romanescu.ro>
2016-05-02 20:25:12 +0300

    Migrate the focal point types and fix NFP migration to use the available values

--- docroot/sites/all/modules/chm/ptk_base/include/PTK.php
+++ docroot/sites/all/modules/chm/ptk_base/include/PTK.php
@@ -4,20 +4,39 @@
 class PTK {
 
   static function getCountryByCode($iso) {
-    $countries = drupal_static(__METHOD__);
-    if (!isset($countries)) {
+    $items = drupal_static(__METHOD__);
+    if (!isset($items)) {
       $q = db_select('field_data_field_country_code');
       $q->fields(NULL, array('entity_id', 'field_country_code_value'));
       $q->condition('entity_type', 'taxonomy_term');
-      $q->condition('bundle', 'countries');
+      $q->condition('bundle', 'items');
       $rows = $q->execute()->fetchAllKeyed();
       foreach ($rows as $k => $code) {
-        $countries[$code] = taxonomy_term_load($k);
+        $items[$code] = taxonomy_term_load($k);
       }
     }
     $k = strtoupper(trim($iso));
-    if (!empty($countries[$k])) {
-      return $countries[$k];
+    if (!empty($items[$k])) {
+      return $items[$k];
+    }
+    return NULL;
+  }
+
+  static function getNFPTypeByAcronym($acronym) {
+    $items = drupal_static(__METHOD__);
+    if (!isset($items)) {
+      $q = db_select('field_data_field_original_id');
+      $q->fields(NULL, array('entity_id', 'field_original_id_value'));
+      $q->condition('entity_type', 'taxonomy_term');
+      $q->condition('bundle', 'taxonomy_nfp_type');
+      $rows = $q->execute()->fetchAllKeyed();
+      foreach ($rows as $k => $code) {
+        $items[$code] = taxonomy_term_load($k);
+      }
+    }
+    $k = strtoupper(trim($acronym));
+    if (!empty($items[$k])) {
+      return $items[$k];
     }
     return NULL;
   }
