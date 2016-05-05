bd3e7b0c7896c8171b8b6e7fa1bf8f6bbc024267
Cristian Romanescu <cristian@romanescu.ro>
2016-05-05 11:42:53 +0300

    Added test alias

--- drush/aliases/aliases.drushrc.php
+++ drush/aliases/aliases.drushrc.php
@@ -1,4 +1,13 @@
 <?php
+$aliases['test'] = array(
+  'uri' => 'test7.chm-cbd.net',
+  'remote-host' => 'www.chm-cbd.net',
+  'remote-user' => 'please-fill-in-local-aliases',
+  'root' => 'please-fill-in-local-aliases',
+  'path-aliases' => array(
+    '%files' => 'sites/all/files',
+  ),
+);
 $aliases['prod'] = array(
   'uri' => 'www.chm-cbd.net',
   'remote-host' => 'www.chm-cbd.net',
