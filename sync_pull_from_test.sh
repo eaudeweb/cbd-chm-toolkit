eb595faaec3363e9fa860707fa026e6ddbf0dea5
Cristian Romanescu <cristian@romanescu.ro>
2016-05-06 00:27:58 +0300

    Added shortcut scripts to sync instances

---
+++ sync_pull_from_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "      _               _              _ _    _ _   "
+echo " THE | | CBD         | |            | | |  (_) |  "
+echo "  ___| |__  _ __ ___ | |_ ___   ___ | | | ___| |_ "
+echo " / __| '_ \| '_ \` _ \| __/ _ \ / _ \| | |/ / | __|"
+echo "| (__| | | | | | | | | || (_) | (_) | |   <| | |_ "
+echo " \___|_| |_|_| |_| |_|\__\___/ \___/|_|_|\_\_|\__|"
+echo "                        kool stuff is happenning..."
+echo ""
+
+cd docroot/
+drush sql-drop -y
+drush sql-sync @prod @self -y
+drush vset environment dev
+drush prepare-dev -y
---
+++ sync_test_from_prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "      _               _              _ _    _ _   "
+echo " THE | | CBD         | |            | | |  (_) |  "
+echo "  ___| |__  _ __ ___ | |_ ___   ___ | | | ___| |_ "
+echo " / __| '_ \| '_ \` _ \| __/ _ \ / _ \| | |/ / | __|"
+echo "| (__| | | | | | | | | || (_) | (_) | |   <| | |_ "
+echo " \___|_| |_|_| |_| |_|\__\___/ \___/|_|_|\_\_|\__|"
+echo "                        kool stuff is happenning..."
+echo ""
+
+cd docroot/
+drush @test sql-drop -y
+drush sql-sync @prod @test -y
+drush @test prepare-test -y
