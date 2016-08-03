0790f83c1c2057e3308fd305e6c4f2ff4ecb8425
Cristian Romanescu <cristian@romanescu.ro>
2016-08-03 11:46:43 +0300

    Added check to avoid dropping test/production databases

--- bin/sync_local_test.sh
+++ bin/sync_local_test.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo "      _               _              _ _    _ _   "
-echo " THE | | CBD         | |            | | |  (_) |  "
-echo "  ___| |__  _ __ ___ | |_ ___   ___ | | | ___| |_ "
-echo " / __| '_ \| '_ \` _ \| __/ _ \ / _ \| | |/ / | __|"
-echo "| (__| | | | | | | | | || (_) | (_) | |   <| | |_ "
-echo " \___|_| |_|_| |_| |_|\__\___/ \___/|_|_|\_\_|\__|"
-echo "                        kool stuff is happenning..."
-echo ""
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[1;37m'
+
+echo -e "${GREEN}      _               _              _ _    _ _   "
+echo -e " THE | | CBD         | |            | | |  (_) |  "
+echo -e "  ___| |__  _ __ ___ | |_ ___   ___ | | | ___| |_ "
+echo -e " / __| '_ \| '_ \` _ \| __/ _ \ / _ \| | |/ / | __|"
+echo -e "| (__| | | | | | | | | || (_) | (_) | |   <| | |_ "
+echo -e " \___|_| |_|_| |_| |_|\__\___/ \___/|_|_|\_\_|\__|"
+echo -e "                        kool stuff is happenning..."
+echo -e ""
+
+env=`drush vget --exact environment`
+if [ $env != 'dev' ]; then
+  echo -e "${RED}ERROR: Refusing to drop the current database. Please set environment to 'dev' (drush vset environment dev)\n";
+  exit -1
+fi
 
 cd ../docroot/
 drush sql-drop -y
