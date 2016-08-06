be6052c59413d812ce461fb7741ecaaf046042e3
Cristian Romanescu <cristian@romanescu.ro>
2016-08-06 23:19:50 +0300

    Fixed bug in sync scripts

--- bin/sync_local_test.sh
+++ bin/sync_local_test.sh
@@ -14,7 +14,7 @@ echo -e "                        kool stuff is happenning..."
 echo -e ""
 
 env=`drush vget --exact environment`
-if [ $env != 'dev' ]; then
+if [ "$env" != 'dev' ]; then
   echo -e "${RED}ERROR: Refusing to drop the current database. Please set environment to 'dev' (drush vset environment dev)\n";
   exit -1
 fi
