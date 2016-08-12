1eeb51f5ed74ca29232421835b8ad5adf3eb848d
Cristian Romanescu <cristian@romanescu.ro>
2016-08-12 14:32:53 +0300

    Added no-image class to the projects view without images

---
+++ docroot/sites/all/themes/chm_theme_kit/templates/views-view-unformatted--projects--page.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+<?php
+  $data = $view->result[$id];
+  if (empty($data->field_field_image)) {
+    $classes_array[$id] .= ' no-image';
+  }
+?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
