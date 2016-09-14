63e5dd5d26fbb8996dfaeecb9d8e05a674043fc4
marapredescu1 <mara.predescu@eaudeweb.ro>
2016-09-14 17:16:11 +0300

    Added base multilevel submenus styling

--- docroot/sites/all/themes/chm_theme_kit/js/script.js
+++ docroot/sites/all/themes/chm_theme_kit/js/script.js
@@ -1,10 +1,16 @@
-jQuery(document).ready(function ($) {
-   $('.dropdown-toggle', '#navbar').removeAttr('data-toggle');
-});
+// jQuery(document).ready(function ($) {
+//    $('.dropdown-toggle', '#navbar').removeAttr('data-toggle');
+// });
 (function($){
-	$(document).ready(function(){
-	  if(document.getElementsByTagName("HTML")[0].dir == "rtl"){
-	    $('#slick-views-home-page-image-slider-1-slider').slick('slickSetOption', 'rtl', true, true);;
-	  }
+	$(document).ready(function() {
+		if(document.getElementsByTagName("HTML")[0].dir == "rtl") {
+			$('#slick-views-home-page-image-slider-1-slider').slick('slickSetOption', 'rtl', true, true);
+		}
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
 	});
 })(jQuery);
