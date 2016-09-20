ae15b16eb6318a776a4555a45c9f733a6c61d346
Cristian Romanescu <cristian@romanescu.ro>
2016-09-20 19:39:19 +0300

    Cleanup debug leftovers

--- docroot/sites/all/themes/chm_theme_kit/templates/field--field-event-status--event.tpl.php
+++ docroot/sites/all/themes/chm_theme_kit/templates/field--field-event-status--event.tpl.php
@@ -44,20 +44,12 @@
  * @ingroup themeable
  */
 ?>
-<?php
-  if (!empty($element['#object']->tid)) {
-    $tid = $element['#object']->tid;
-    dpm($tid);
-    $classes .= ' field-event-status-' . $tid;
-  }
-  dpm($classes);
-?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
-  <?php foreach ($items as $delta => $item): dpm($element); ?>
+  <?php foreach ($items as $delta => $item): ?>
     <?php $tid = $element['#items'][$delta]['tid']; ?>
     <div class="field-item field-event-status-<?php print $tid; ?> <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
   <?php endforeach; ?>
