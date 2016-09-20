14342ed519ca5ab885787b971b1a4de23b496844
marapredescu1 <mara.predescu@eaudeweb.ro>
2016-09-20 12:37:36 +0300

    Removed container from gallery

--- docroot/sites/all/themes/chm_theme_kit/templates/field--field_images--event.tpl.php
+++ docroot/sites/all/themes/chm_theme_kit/templates/field--field_images--event.tpl.php
@@ -1,4 +1,4 @@
-<div class="container <?php print $classes; ?>"<?php print $attributes; ?>>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
