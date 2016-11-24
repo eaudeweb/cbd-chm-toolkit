11042551bb714e956c8c6e91cd4faeec5fd52e02
marapredescu1 <mara.predescu@eaudeweb.ro>
2016-11-24 16:25:11 +0200

    Added select2 script

--- docroot/sites/all/modules/chm/chm_search/js/chm_search_hide_submit.js
+++ docroot/sites/all/modules/chm/chm_search/js/chm_search_hide_submit.js
@@ -1,24 +1,44 @@
-(function($){
-  /**
-   * Hide submit button in select widget facet.
-   **/
-  Drupal.behaviors.ExampleHideSubmit = {
-  attach: function(context) {
-    $('.chm-search-select-facet .ctools-auto-submit-click:not(.chm_search-hide-submit-processed)', context)
-    .addClass('chm_search-hide-submit-processed').hide();
-  }}
-
+(function($) {
+  $(document).ready(function() {
+      // Select2
+      var initialSelects = $('[id^=edit-select]');
+      var placeholderValue = initialSelects.eq(0).data('placeholder');
+      initialSelects.each(function() {
+        var self = $(this);
+        self.css('width', '100%').removeClass('form-control').select2({
+          allowClear: true,
+          placeholder: placeholderValue,
+          escapeMarkup: function (text) { return text; }
+        });
+      });
+      // $('[id^=s2id_autogen]','#chm-search-type')
+      $('.select2-input').each(function () {
+        $(this)[0].placeholder = placeholderValue;
+      });
+      $('.ctools-auto-submit-click:not(.chm_search-hide-submit-processed)').addClass('chm_search-hide-submit-processed').hide();
+    });
 })(jQuery);
+// (function($){
+//   /**
+//    * Hide submit button in select widget facet.
+//    **/
+//   Drupal.behaviors.ExampleHideSubmit = {
+//   attach: function(context) {
+//     $('.chm-search-select-facet .ctools-auto-submit-click:not(.chm_search-hide-submit-processed)', context)
+//     .addClass('chm_search-hide-submit-processed').hide();
+//   }}
 
-(function ($, window, document) {
+// })(jQuery);
 
-  Drupal.Select2.functionsScope.chm_search_format_selection = function (term) {
+// (function ($, window, document) {
 
-    if (term.hover_title) {
-      return term.hover_title;
-    }
+//   Drupal.Select2.functionsScope.chm_search_format_selection = function (term) {
 
-    return term.text;
-  };
+//     if (term.hover_title) {
+//       return term.hover_title;
+//     }
 
-})(jQuery, window, document);
+//     return term.text;
+//   };
+
+// })(jQuery, window, document);
